29e590c09a2412af6a261705403292ea083cd2e2
bufleek <brunookiprop@gmail.com>
2023-12-30 23:14:19 +0300

    removed next/font usage in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
 import { fetchAppConfigs } from '../data/models/app_configs'
 import { AppConfigsProvider } from '../components/providers'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: process.env.APP_NAME,
@@ -25,7 +25,8 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
+      {/* <body className={inter.className}> */}
         <AppConfigsProvider value={appConfigs}>
           {children}
         </AppConfigsProvider>
