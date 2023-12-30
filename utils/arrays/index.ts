45251b1682df8cc85e54fe20b7ff4404165a24ae
bufleek <brunookiprop@gmail.com>
2023-12-30 09:19:25 +0300

    updated paths in tsconfig

--- utils/arrays/index.ts
+++ utils/arrays/index.ts
@@ -3,7 +3,7 @@
  * @param {Array} arr - The array to be shuffled.
  * @returns {Array} - The shuffled array.
  */
-export function shuffle(arr) {
+export function shuffle(arr: any[]) {
     const _arr = arr.slice()
     for (let i = _arr.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
